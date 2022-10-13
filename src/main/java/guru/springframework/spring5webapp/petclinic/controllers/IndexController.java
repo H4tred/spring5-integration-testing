49be92bf355a3123da252648ef2056c5736a5246
milan <milan.htc@outlook.com>
2022-10-13 19:34:40 +0200

    section5-timeouts

--- src/main/java/guru/springframework/spring5webapp/petclinic/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5webapp/petclinic/controllers/IndexController.java
@@ -1,12 +1,14 @@
 package guru.springframework.spring5webapp.petclinic.controllers;
 
+import guru.springframework.spring5webapp.petclinic.exceptions.ValueNotFoundException;
+
 public class IndexController {
 
     public String index() {
         return "index";
     }
 
-    public String oupsHandler() {
-        return "notimplemented";
+    public String oupsHandler() throws ValueNotFoundException {
+        throw new ValueNotFoundException();
     }
 }
--- src/test/java/guru/springframework/spring5webapp/petclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/spring5webapp/petclinic/controllers/IndexControllerTest.java
@@ -2,7 +2,11 @@ package guru.springframework.spring5webapp.petclinic.controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import guru.springframework.spring5webapp.petclinic.exceptions.ValueNotFoundException;
+import java.time.Duration;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class IndexControllerTest {
@@ -21,7 +25,27 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("MyTestMethod ☺")
     void oupsHandler() {
-        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive message to build for my test!");
+        //assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive message to build for my test!");
+        assertThrows(ValueNotFoundException.class, () -> {controller.oupsHandler();});
+    }
+
+    @Test
+    @Disabled("Demo of timeout")
+    void testTimeout() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+            System.out.println("I got here");
+        });
+    }
+
+    @Test
+    @Disabled("Demo of timeout - in thread")
+    void testTimeoutPreempt() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(5000);
+            System.out.println("I got here **********");
+        });
     }
 }
